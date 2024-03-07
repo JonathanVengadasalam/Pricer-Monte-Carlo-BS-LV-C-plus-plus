cb1085569790f0ced432b2c63a4772a03cf54b98
Jonathan <jonathan.vengadasalam@gmail.com>
2024-03-07 10:00:09 +0100

    Update DiffusionDupire.cpp

--- DiffusionDupire.cpp
+++ DiffusionDupire.cpp
@@ -12,16 +12,18 @@ double DiffusionDupire::drift(double X_t, double t) {
 }
 
 double DiffusionDupire::volatility(double X_t, double _t) {
-    long xInd = firstInfIndex(x, xLen, X_t);
-    long tInd = firstInfIndex(t, tLen, _t);
-    double x0 = x[xInd];
-    double t0 = t[tInd];
-    double x1 = x[xInd + 1];
-    double t1 = t[tInd + 1];
+    long xInd0 = firstInfIndex(x, xLen, X_t);
+    long tInd0 = firstInfIndex(t, tLen, _t);
+    long xInd1 = xInd0 + 1;
+    long tInd1 = tInd0 + 1;
+    double x0 = x[xInd0];
+    double t0 = t[tInd0];
+    double x1 = x[xInd1];
+    double t1 = t[tInd1];
 
     return X_t * (
-           sigma[x0 + tLen * t0] * (x1 - X_t) * (t1 - _t)
-         + sigma[x1 + tLen * t0] * (x0 - X_t) * (t1 - _t)
-         + sigma[x0 + tLen * t1] * (x1 - X_t) * (t0 - _t)
-         + sigma[x1 + tLen * t1] * (x0 - X_t) * (t0 - _t));
+           sigma[xInd0 + tLen * tInd0] * (x1 - X_t) * (t1 - _t)
+         + sigma[xInd1 + tLen * tInd0] * (x0 - X_t) * (t1 - _t)
+         + sigma[xInd0 + tLen * tInd1] * (x1 - X_t) * (t0 - _t)
+         + sigma[xInd1 + tLen * tInd1] * (x0 - X_t) * (t0 - _t));
 }
