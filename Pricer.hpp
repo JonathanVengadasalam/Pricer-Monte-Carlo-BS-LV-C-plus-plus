71a6ab3d04db83bf81f61d52844c40a6beb563ac
Jonathan <jonathan.vengadasalam@gmail.com>
2024-03-12 14:14:11 +0100

    Update Pricer.hpp

--- Pricer.hpp
+++ Pricer.hpp
@@ -1,4 +1,5 @@
 #include "DiffusionModel.hpp"
 #include "EuropeanCall.hpp"
+#include "RandomNormal.hpp"
 
-void MonteCarlo(double prices[], long pricesLen, DiffusionModel &mod, EuropeanCall call, long timeSteps);
+void MonteCarlo(double prices[], long pricesLen, RandomNormal& generator, DiffusionModel& model, EuropeanCall call, long timeSteps);
