74f5325f607e56b339a1bd71d6dab220f20ad002
Jonathan <jonathan.vengadasalam@gmail.com>
2024-03-14 13:07:44 +0100

    Update RandomUniform.hpp

--- RandomGeneratorHpp/RandomUniform.hpp
+++ RandomGeneratorHpp/RandomUniform.hpp
@@ -5,7 +5,10 @@
 
 class RandomUniform : public RandomGenerator {
 public:
+	RandomUniform* shuffler;
+
 	RandomUniform(long _N);
+	RandomUniform(long _N, RandomUniform* _shuffler);
 	double probability(double value) override;
 	void generateArray(double values[], long valuesLen) override;
 	void shuffle(double values[], long valuesLen);
