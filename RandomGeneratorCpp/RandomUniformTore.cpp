e288273ce00693974fafc39d08d586862ed3f6d5
Jonathan <jonathan.vengadasalam@gmail.com>
2024-03-14 13:11:57 +0100

    Update RandomUniformTore.cpp

--- RandomGeneratorCpp/RandomUniformTore.cpp
+++ RandomGeneratorCpp/RandomUniformTore.cpp
@@ -5,6 +5,10 @@ RandomUniformTore::RandomUniformTore(long _prime) : RandomUniform(0) {
 	prime = _prime;
 }
 
+RandomUniformTore::RandomUniformTore(long _prime, RandomUniform* _shuffler) : RandomUniform(0, _shuffler) {
+	prime = _prime;
+}
+
 double RandomUniformTore::computeValue() {
 	return N * std::sqrt(prime) - std::floor(N * std::sqrt(prime));
 }
